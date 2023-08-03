1a41ceec553e8ef124bcd4681d3c041493ef512a
nablanerd <nablanerd@yahoo.com>
2023-08-03 11:29:19 +0200

    updating login and register process

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+import {Router} from "@angular/router"
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -10,7 +12,7 @@ export class RegisterComponent {
 
   registerForm!: FormGroup; 
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder, private router: Router) { }
 
   ngOnInit() {
     this.registerForm = this.formBuilder.group({
@@ -38,6 +40,9 @@ export class RegisterComponent {
     .then(data => {
   
       console.log(data);
+
+      this.router.navigate(['/login'])
+
     })
     .catch(error => {
       console.error('Error:', error);
